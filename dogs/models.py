d8ccba4afaf476bb66273b3566b0493e7ce7bb37
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-01-04 17:37:08 +0000

    Add Dog model with fields for owner, name, age, breed, size, energy level, description, and profile picture; create initial migration

--- dogs/models.py
+++ dogs/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from profiles.models import OwnerProfile
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+ENERGY_LEVEL_CHOICES = [
+    ('couch_potato', '🛋️ Couch potato'),
+    ('chill', '😌 Chill vibes'),
+    ('playful', '🎾 Playful'),
+    ('energetic', '⚡ Energetic'),
+    ('zoomies', '🚀 Full zoomies'),
+]
+
+
+class Dog(models.Model):
+    owner = models.OneToOneField(
+        OwnerProfile,
+        on_delete=models.CASCADE,
+        related_name="dog"
+    )
+
+    name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+    breed = models.CharField(max_length=100)
+
+    size = models.CharField(
+        max_length=10,
+        choices=[
+            ('small', 'Small'),
+            ('medium', 'Medium'),
+            ('large', 'Large'),
+        ]
+    )
+
+    energy_level = models.CharField(
+        max_length=12,
+        choices=ENERGY_LEVEL_CHOICES
+    )
+
+    description = models.TextField(blank=True)
+
+    profile_picture = CloudinaryField('dog picture', blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
