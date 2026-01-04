d61278fbc85acc805fc0cee5857b81c7796e1e8c
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-01-04 16:35:34 +0000

    Add OwnerProfile model with migration and update admin registration; include additional requirements

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class OwnerProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    display_name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+    location = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.display_name
