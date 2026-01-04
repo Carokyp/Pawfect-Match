19afede2a99a6804efbf775f778dd6bdb3701926
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-01-04 17:47:34 +0000

    Add Connection model with from_dog and to_dog fields; register in admin and create initial migration

--- connections/models.py
+++ connections/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from dogs.models import Dog
 
-# Create your models here.
+
+class Connection(models.Model):
+    from_dog = models.ForeignKey(
+        Dog,
+        related_name="sent_connections",
+        on_delete=models.CASCADE
+    )
+    to_dog = models.ForeignKey(
+        Dog,
+        related_name="received_connections",
+        on_delete=models.CASCADE
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.from_dog} ❤️ {self.to_dog}"
