41b376a32198e6a7d2f88c3cb9fdbc64d3ef12be
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-01-12 10:54:21 +0000

    Implement user registration view with form handling and profile creation

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,25 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegisterForm
+from profiles.models import OwnerProfile
 
 
-def test_view(request):
-    return HttpResponse("Welcome to Pawfect Match!")
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+
+            OwnerProfile.objects.create(
+                user=user,
+                display_name=user.username
+            )
+
+            login(request, user)
+            return redirect("home")
+    else:
+        form = RegisterForm()
+
+    return render(request, "accounts/register.html", {"form": form})
