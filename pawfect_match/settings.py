23918e132eeb3343248df0675a37a6839d37f677
Carokyp <97530091+Carokyp@users.noreply.github.com>
2026-03-02 15:15:41 +0000

    Import static files

--- pawfect_match/settings.py
+++ pawfect_match/settings.py
@@ -140,6 +140,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+WHITENOISE_MANIFEST_STRICT = False
 
 # Database & File Storage
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
